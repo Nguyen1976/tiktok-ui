1abc19caa661b5e39209360e5833889bf37ab369
Nguyen1976 <Nguyen2202794@gmail.com>
2024-08-10 21:19:42 +0700

    handle header-search(part 2)

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,22 +1,23 @@
-import classNames from "classnames/bind";
-import styles from './AccountItem.module.scss'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
-import Image from '~/components/Image'
-const cx = classNames.bind(styles)
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import styles from './AccountItem.module.scss';
+import Image from '~/components/Image';
+const cx = classNames.bind(styles);
 
-function AccountItem() {
-    return ( 
-        <div className={cx('wrapper')}>
-            <Image className={cx('avatar')} src='https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/abd95ae25a622e8cb244f24cdefce94f.jpeg?lk3s=30310797&nonce=32640&refresh_token=e7af3c6e3e67ace8903858f321a4ecd9&x-expires=1723215600&x-signature=rS6swTyf9MgCe5D9AyuAok%2F7PcE%3D&shp=30310797&shcp=-' alt="Hoaa"/>
+function AccountItem({ data }) {
+    return (
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Nguyen Van A</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>
+                    <span>{data.full_name} </span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>Username</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,9 +9,9 @@ import Search from '~/pages/Search';
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
-    { path: '/profile', component: Profile },
+    { path: '/:nickname', component: Profile },
     { path: '/upload', component: Upload, layout: HeaderOnly },
-    { path: '/search', component: Search },
+    { path: '/search', component: Search, layout: null },
 ];
 
 const privateRoutes = [];
