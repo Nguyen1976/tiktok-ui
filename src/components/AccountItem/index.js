ae6ec08ffe4918ec0cbf20f9a635baaba99ee13a
Nguyen1976 <Nguyen2202794@gmail.com>
2024-08-22 10:28:12 +0700

    use libary proptypes

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
@@ -19,6 +20,10 @@ function AccountItem({ data }) {
             </div>
         </Link>
     );
+}
+
+AccountItem.propTypes = {
+    data: PropTypes.object,
 }
 
 export default AccountItem;
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { useState } from 'react';
 // import Tippy from '@tippyjs/react';
@@ -41,7 +42,7 @@ function Menu({ children, items, hideOnClick = false, onChange, ...passProps }) 
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
                     <PopperWrapper className={cx('menu-popper')}>
-                        {history.length > 1 && <Header title="Language" onBack={() => {
+                        {history.length > 1 && <Header title={current.title} onBack={() => {
                             setHistory(prev => prev.slice(0, -1))
                         }}/>}
                        <div className={cx('menu-body')}> {renderItems()}</div>
@@ -54,6 +55,13 @@ function Menu({ children, items, hideOnClick = false, onChange, ...passProps }) 
             {children}
         </Tippy>
     );
+}
+
+Menu.prototype = {
+    children : PropTypes.node.isRequired,
+    items : PropTypes.array,
+    hideOnClick : PropTypes.bool,
+    onChange : PropTypes.func,
 }
 
 export default Menu;
