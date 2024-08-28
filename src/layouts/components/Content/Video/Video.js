4056a4447eca21a6376bbf3036f205e6e6bd4174
Nguyen1976 <Nguyen2202794@gmail.com>
2024-08-28 23:00:41 +0700

    video function demo

--- src/layouts/components/Content/Video/Video.js
+++ src/layouts/components/Content/Video/Video.js
@@ -1,23 +1,41 @@
 import classNames from "classnames/bind";
 import ReactPlayer from 'react-player'
+import { useState } from 'react'
 
 import styles from './Video.module.scss';
 import video from '~/assets/videos/test.mp4'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faMusic } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
 
 function Video() {
+
+    const [ isHover, setIsHover ] = useState(false);
+
+    const handleHoverIn = () => {
+        setIsHover(true);
+    }
+
+    const handleHoverOut = () => {
+        setIsHover(false);
+    }
+
     return (  
-        <div className={cx('video')}>
+        <div className={cx('video')}
+            onMouseEnter={() => handleHoverIn()}
+            onMouseLeave={() => handleHoverOut()}
+        >
             <ReactPlayer 
                 className={cx('video')}
                 url={video} 
-                controls={false} 
+                controls={isHover} 
                 width='100%' 
                 height='auto'
             />
-            <div className={cx('overlay')}>
+            <div className={cx('overlay')} 
+                onMouseEnter={() => handleHoverIn()}
+                onMouseLeave={() => handleHoverOut()}
+            >
                 <h3 className={cx('name')}>
                     <span>ggs68korea.official</span> 
                     <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>
@@ -31,6 +49,12 @@ function Video() {
                     <a href="/" className={cx('hastag')}>#ggs68</a>
                     <a href="/" className={cx('hastag')}>#ggs68</a>
                     <a href="/" className={cx('hastag')}>#ggs68</a>
+                </div>
+                <div className={cx('music')}>
+                    <FontAwesomeIcon icon={faMusic}/>
+                    <div className={cx('music-name')}>
+                        Nhạc nền - Bnhi
+                    </div>
                 </div>
             </div>
         </div>
